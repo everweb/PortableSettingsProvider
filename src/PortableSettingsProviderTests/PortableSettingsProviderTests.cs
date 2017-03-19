f8b7d7ab491b605f1a5efc23edca46164ed1eda7
Rupert Bruce <rupert.bruce@titletrading.com>
2017-03-18 22:34:36 -0400

    Remove WinForm dependency Allow MachineNames beginning with numbers by prefixing (including check to see if it's necessary) Add setter for ApplicationName since it is required

---
+++ src/PortableSettingsProviderTests/PortableSettingsProviderTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using crdx.Settings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+using System.Configuration;
+namespace crdx.Settings.Tests
+{
+	[TestClass()]
+	public class PortableSettingsProviderTests
+	{
+
+		[TestMethod()]
+		public void SetPropertyValuesTest()
+		{			
+			string expected = "Chips";
+			Properties.Settings.Default.TestSetting = expected;
+			string actual = Properties.Settings.Default.TestSetting;
+			Assert.AreEqual(expected, actual);
+		}
+
+
+		[TestMethod()]
+		public void GetBlankXmlDocumentTest()
+		{
+			PortableSettingsProvider psp = new PortableSettingsProvider();
+			XmlDocument blank = psp.GetBlankXmlDocument();
+			string expected = @"<?xml version=""1.0"" encoding=""utf-8""?><settings />";
+			Assert.AreEqual(expected, blank.InnerXml);
+		}
+
+		[TestMethod()]
+		public void ResetTest()
+		{
+			//TEST//PortableSettingsProvider.OverrideMachineName("_the_machine_");
+			string expected = "Chips";
+			Properties.Settings.Default.TestSetting = expected;
+			string actual = Properties.Settings.Default.TestSetting;
+			Assert.AreEqual(expected, actual);
+			//Save a known setting
+			Properties.Settings.Default.Save();
+			
+			//Set a different value
+			Properties.Settings.Default.TestSetting = "unexpected";
+
+			//Reset
+			Properties.Settings.Default.Reset();
+
+			//Verify that the known value is restored
+			Assert.AreEqual(expected, actual);
+		}	
+
+	}
+}
---
+++ src/PortableSettingsProviderTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PortableSettingsProviderTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Everweb")]
+[assembly: AssemblyProduct("PortableSettingsProviderTests")]
+[assembly: AssemblyCopyright("Copyright © Everweb 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b311af6-1d52-4653-8526-2ddb3282f83a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
